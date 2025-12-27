4cb4a4e23a1ef7817c0964e6d519bade175aff69
ranvi2006 <rajurk2002@gmail.com>
2025-12-27 13:27:25 +0530

    corection

--- backend/src/models/userCourses.routes.js
+++ backend/src/models/userCourses.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/user");
+const User = require("./User");
 const userAuth = require("../middleware/userAuth.middleware");
 
 const router = express.Router();
